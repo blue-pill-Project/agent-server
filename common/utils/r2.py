bff7cf2691ca0ecf7da546ed9da6ae9a9d682bab
merx88 <wjang07@gmail.com>
2026-08-17 22:13:16 +0900

    chore: 환경 변수 이름 수정

--- common/utils/r2.py
+++ common/utils/r2.py
@@ -7,9 +7,9 @@ from common.config import settings
 def get_r2_client():
     return boto3.client(
         "s3",
-        endpoint_url=f"https://{settings.R2_ACCOUNT_ID}.r2.cloudflarestorage.com",
-        aws_access_key_id=settings.R2_ACCESS_KEY,
-        aws_secret_access_key=settings.R2_SECRET_KEY,
+        endpoint_url=f"https://{settings.CLOUDFLARE_R2_ACCOUNT_ID}.r2.cloudflarestorage.com",
+        aws_access_key_id=settings.CLOUDFLARE_R2_ACCESS_KEY,
+        aws_secret_access_key=settings.CLOUDFLARE_R2_SECRET_KEY,
         region_name="auto",
         config=Config(signature_version="s3v4"),
     )
