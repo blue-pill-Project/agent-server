e604b6f9de73577996af6cddc7964bcdeb01c4ce
merx88 <wjang07@gmail.com>
2026-08-29 12:57:49 +0900

    test: 테스트logging config 설정

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import logging
+
+
+def pytest_configure():
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("httpcore").setLevel(logging.WARNING)
+    logging.getLogger("openai").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("huggingface_hub").setLevel(logging.WARNING)
+    logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
